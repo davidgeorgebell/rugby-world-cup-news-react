c6f592e0b34723c9c1ee0b0dff5de9dc0788623e
davidgeorgebell <bell.dave93@gmail.com>
2019-09-25 10:03:07 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: 'Steve Jobs dies',
+    url: 'https://apple.com',
+    author: 'wasniac',
+    num_comments: 10,
+    points: 8,
+    objectID: 2,
+  },
+];
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+       
+       list.map(item =>
+        <>
+        <span>
+          <a href={item.url}><h3>{item.title}</h3></a>
+        </span>
+        <span>{item.points}</span>
+        <span><p>{item.author}</p></span>
+       </>
+       )
+       
+     }
     </div>
   );
 }
